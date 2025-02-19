8f57ba716198cb313bd363600d64c91b0efa1a5f
IrynaT15 <irina.tainikova@gmail.com>
2025-02-19 14:24:02 +0100

    Update cows_and_bulls.py

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -20,25 +20,24 @@ winner = 0
 while winner == 0:
     if (len(gamer_number) == 4 and gamer_number.isdigit() and
        len(gamer_number) == len(set(gamer_number))):
-       if gamer_number == secret_number:
-           winner = 1
-       else:
-           secret_number_list = [int(x) for x in secret_number]
-            gamer_number_list = [int(x) for x in gamer_number]
-            bull = 0
-            cow = 0
-            for x in gamer_number_list:
-                for y in secret_number_list:
-                    if x == y:
-                        if gamer_number_list.index(x) == secret_number_list.index(y):
-                            bull += 1
-                        else:
-                            cow += 1
-            print(f"Your result: {cow} cows, {bull} bulls")
-            bull, cow = 0, 0
-            print("Try again!")
-            gamer_number = input("Enter your number: ")
-            winner = 0
+           if gamer_number == secret_number:
+               winner = 1
+           else:
+               secret_number_list = [int(x) for x in secret_number]
+               gamer_number_list = [int(x) for x in gamer_number]
+               bull = 0
+               cow = 0
+               for x in gamer_number_list:
+                   for y in secret_number_list:
+                       if x == y:
+                           if gamer_number_list.index(x) == secret_number_list.index(y):
+                               bull += 1
+                           else:
+                               cow += 1
+               print(f"Your result: {cow} cows, {bull} bulls")
+               bull, cow = 0, 0
+               gamer_number = input("Try again! Enter your number: ")
+               winner = 0
     else:
         print("Invalid number.")
         gamer_number = input("Enter another number: ")
