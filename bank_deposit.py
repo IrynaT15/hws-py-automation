d0e732ad255569ebc64741e21812a5609c4a89a2
IrynaT15 <irina.tainikova@gmail.com>
2025-03-04 09:52:52 +0100

    Completed tasks for HW12. Edited

--- bank_deposit.py
+++ bank_deposit.py
@@ -2,13 +2,20 @@ class Bank_deposit:
     interest_rate = 0.1
     clients_ids = []
 
+    def __init__(self):
+        self.client_id = None
+        self.client_name = None
+        self.is_deposit_open = False
+        self.start_balance = None
+        self.years = None
+        self.interest_frequency = None
+
     def register_client(self, client_id, client_name):
         if client_id in Bank_deposit.clients_ids:
             return f"{client_id}: Warning! Client is already registered"
         self.client_id = client_id
         self.client_name = client_name
         Bank_deposit.clients_ids.append(self.client_id)
-        self.is_deposit_open = False
         return f"{self.client_id}: Success! New client is registered"
 
     def open_deposit_account(self, client_id, start_balance, years, interest_frequency):
@@ -27,7 +34,7 @@ class Bank_deposit:
         self.client_id = client_id
         if self.client_id not in Bank_deposit.clients_ids:
             return "Warning! Client not registered."
-        if self.is_deposit_open == False:
+        if not self.is_deposit_open:
             return f"{self.client_id}: Warning! No open deposit account."
         final_balance = self.start_balance * ((1 + Bank_deposit.interest_rate / self.interest_frequency) ** (self.interest_frequency * self.years))
         return f"Final balance: {round(final_balance, 2)}"
@@ -45,14 +52,8 @@ class Bank_deposit:
 
 bank = Bank_deposit()
 print(bank.register_client("0000001", "Iryna Tain"))
-print(bank.open_deposit_account("0000001", 1000, 1, 12))
+print(bank.open_deposit_account("0000002", 1000, 1, 12))
 print(bank.open_deposit_account("0000001", 1000, 1, 12))
 print(bank.calc_deposit_interest_rate("0000001"))
 print(bank.close_deposit("0000001"))
 print(bank.register_client("0000001", "Iryna Tain"))
-
-
-
-
-
-
