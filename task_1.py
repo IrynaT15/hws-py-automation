7b89f65a1758ac66ada0e3b9768debc438974b6f
IrynaT15 <irina.tainikova@gmail.com>
2025-03-18 17:19:56 +0100

    Task1

---
+++ task_1.py
@@ -0,0 +1,9 @@
+string = "This is8a sentence for the test It contains more than 15 characters"
+
+print(string[0])     # Извлеките из строки первый символ
+print(string[-1])    # Извлеките из строки последний символ
+print(string[2])     # Извлеките из строки третий с начала
+print(string[-3])    # Извлеките из строки третий с конца
+print(len(string))   # Измерьте длину вашей строки
+print(string[::-1])  # Переверните строку
+print(string[:8])    # Извлеките первые восемь символов
